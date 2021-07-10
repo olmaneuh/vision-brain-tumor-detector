a434557aae2303c5c762bb1f8b9191a122940c3f
Olman Ureña <A9XH7ZZ@mmm.com>
2021-07-09 21:35:50 -0600

    Web App: Create the ImageClassifier class with the preprocess image method and predict method

--- brain-tumor-detector/classifier.py
+++ brain-tumor-detector/classifier.py
@@ -1,4 +1,46 @@
+# Copyright 2021 Olman Ureña
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import keras
 import numpy as np
 
 from PIL import Image, ImageOps
+
+class ImageClassifier:
+    '''
+    TODO
+    '''
+
+    def __init__(self, image_shape=(224, 224, 3), image_size=(224, 224)):
+        '''
+        TODO
+        '''
+        self.model = keras.models.load_model('../model/model.h5')
+        self.image_shape = image_shape
+        self.image_size = image_size
+
+    def preprocess_image(self, image):
+        '''
+        TODO
+        '''
+        image = ImageOps.fit(image, self.size, Image.ANTIALIAS)
+        image = np.asarray(image)
+        image = (image.astype(np.float32) / 127.0) - 1
+        return image
+
+    def predict(self, image):
+        '''
+        TODO
+        '''
+        return None
